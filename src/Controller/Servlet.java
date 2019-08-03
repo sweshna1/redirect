91f7296c53a8764651462bea7999e1a801336edb
sweshna1 <shweshna12@gmail.com>
2019-08-03 08:21:05 +0545

    redirect

---
+++ src/Controller/Servlet.java
@@ -0,0 +1,18 @@
+package Controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setContentType("index/html");
+        PrintWriter out = response.getWriter();
+        response.sendRedirect("https://www.facebook.com");
+        out.close();
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
